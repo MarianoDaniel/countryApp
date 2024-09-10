162b9d6a465fd7656a71c9d5c6fc301a5aa256eb
Mariano Avico <mavico@ignetworks.com>
2024-09-10 11:43:53 -0300

    [routerLinkActiveOptions]='{exact:true}' para que no quede en active cuando se elijan las demás opciones

---
+++ app.routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/Pages/homePage/homePage.component';
+import { AboutPageComponent } from './shared/Pages/aboutPage/aboutPage.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent
+    },
+
+    {
+        path: 'about',
+        component: AboutPageComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes),
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
